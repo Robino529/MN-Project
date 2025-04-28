7534d6a0c0ba669e46efefafd2c7d6b4f9871fae
Robin Courault <robin.courault@etu.univ-grenoble-alpes.fr>
2025-04-28 08:44:00 +0200

    renamed of convergenceAtteinte.java

---
+++ rl-lib/src/main/java/fr/polytech/mnia/utils/ConvergenceAtteinte.java
@@ -0,0 +1,7 @@
+package fr.polytech.mnia.utils;
+
+public class ConvergenceAtteinte extends RuntimeException {
+	public ConvergenceAtteinte(String message) {
+		super(message);
+	}
+}
--- rl-lib/src/main/java/fr/polytech/mnia/utils/convergenceAtteinte.java
+++
@@ -1,7 +0,0 @@
-package fr.polytech.mnia.utils;
-
-public class convergenceAtteinte extends RuntimeException {
-	public convergenceAtteinte(String message) {
-		super(message);
-	}
-}
